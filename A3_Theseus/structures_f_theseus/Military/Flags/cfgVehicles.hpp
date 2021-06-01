a0d753e61b75e9e1e8a37843a73e3a229085f468
Senicluxus <senicluxus@gmail.com>
2021-06-01 19:57:19 -0400

    Added NAAF Flag Placeable

--- A3_Theseus/structures_f_theseus/Military/Flags/cfgVehicles.hpp
+++ A3_Theseus/structures_f_theseus/Military/Flags/cfgVehicles.hpp
@@ -2,6 +2,30 @@ class CfgVehicles
 {
 	class FlagCarrier;
 	class FlagCarrier_Asym;
+	class Flag_NAAF_F: FlagCarrier
+	{
+		author = $STR_A3_A_Heliotrope;
+		class SimpleObject
+		{
+			eden = false;
+			animate[] =
+			{
+				{flag,0}
+			};
+			hide[] = {};
+			verticalOffset = 3.977;
+			verticalOffsetWorld = 0;
+			init = "''";
+		};
+		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Flag_FIA_F.jpg";
+		scope = public;
+		scopeCurator = public;
+		displayName = $STR_A3_T_CfgVehicles_Flag_NAAF_F0;
+		class EventHandlers
+		{
+			init = "(_this select 0) setFlagTexture '\A3_Theseus\Data_F_Theseus\Flags\flag_NAAF_CO.paa'";
+		};
+	};
 	class Flag_Remnants_F: FlagCarrier
 	{
 		author = $STR_A3_A_Heliotrope;
