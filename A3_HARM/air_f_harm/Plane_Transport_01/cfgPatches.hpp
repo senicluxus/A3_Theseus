0e3ab5f39c7f0640f3eb00bf933f060119b725b4
Senicluxus <senicluxus@gmail.com>
2021-06-01 19:31:48 -0400

    Initial Commit

---
+++ A3_HARM/air_f_harm/Plane_Transport_01/cfgPatches.hpp
@@ -0,0 +1,11 @@
+class CfgPatches
+{
+	class A3_HARM_Air_F_HARM_Plane_Transport_01
+	{
+		addonRootClass = A3_HARM_Air_F_HARM;
+		requiredAddons[] = {A3_HARM_Air_F_HARM};
+		requiredVersion = 0.1;
+		units[] =   {/* Automated */};
+		weapons[] = {/* Automated */};
+	};
+};
