9d0f7af08288c5870f4a72c276384fa0e9adb11b
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-08-28 14:29:17 +0100

    NAAF Prowler work begins

---
+++ A3_Theseus/soft_f_theseus/LSV_01/cfgPatches.hpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+	class A3_Theseus_Soft_F_Theseus_LSV_01
+	{
+		addonRootClass = A3_Theseus_Soft_F_Theseus;
+		requiredAddons[] = {A3_Theseus_Soft_F_Theseus};
+		requiredVersion = 0.1;
+		units[] =
+        {
+            /* From "cfgNAAF.hpp" */
+            
+        };
+		weapons[] = {/* Automated */};
+	};
+};
