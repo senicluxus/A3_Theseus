0f8df48ac8b3af64c82ac958025e983b82ec4fd6
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-11-05 22:43:52 -0700

    NAAF Desert Fatigues Added
    Added NAAF desert fatigues as well as the desert service helmet texture.

--- A3_Theseus/characters_f_theseus/Uniforms/cfgNAAF.hpp
+++ A3_Theseus/characters_f_theseus/Uniforms/cfgNAAF.hpp
@@ -32,6 +32,40 @@ class Theseus_U_B_NAAF_Uniform_01_shortsleeve_dgtl_F: Uniform_Base
 		mass = 40;
 	};
 };
+class Theseus_U_B_NAAF_Uniform_01_des_F: Uniform_Base
+{
+	author = $STR_A3_A_BranFlakes;
+	scope = public;
+	displayName = $STR_A3_T_CfgWeapons_U_B_NAAF_Uniform_01_des_F0;
+	picture = "\A3\Characters_F_Beta\Data\UI\icon_U_IR_CrewUniform_rucamo_CA.paa";
+	model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+	hiddenSelections[] = {camo};
+	hiddenSelectionsTextures[] = {"\A3\Characters_F\Common\Suitpacks\Data\suitpack_soldier_indep_CO.paa"};
+	class ItemInfo: UniformItem
+	{
+		uniformModel = "-";
+		uniformClass = Theseus_B_NAAF_Uniform_01_des_F;
+		containerClass = Supply40;
+		mass = 40;
+	};
+};
+class Theseus_U_B_NAAF_Uniform_01_shortsleeve_des_F: Uniform_Base
+{
+	author = $STR_A3_A_BranFlakes;
+	scope = public;
+	displayName = $STR_A3_T_CfgWeapons_U_B_NAAF_Uniform_01_shortsleeve_des_F0;
+	picture = "\A3\Characters_F_Beta\Data\UI\icon_U_IR_CombatUniform_rucamo_CA.paa";
+	model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+	hiddenSelections[] = {camo};
+	hiddenSelectionsTextures[] = {"\A3\Characters_F\Common\Suitpacks\Data\suitpack_soldier_indep_CO.paa"};
+	class ItemInfo: UniformItem
+	{
+		uniformModel = "-";
+		uniformClass = Theseus_B_NAAF_Uniform_01_shortsleeve_des_F;
+		containerClass = Supply40;
+		mass = 40;
+	};
+};
 class Theseus_U_B_NAAF_Officer_dgtl_F: Uniform_Base
 {
 	author = $STR_A3_A_Heliotrope;
--- A3_Theseus/characters_f_theseus/cfgUniforms.hpp
+++ A3_Theseus/characters_f_theseus/cfgUniforms.hpp
@@ -30,3 +30,17 @@ class Theseus_B_NAAF_Officer_dgtl_F: I_officer_F
 		"\A3_Theseus\Characters_F_Theseus\Uniforms\Data\theseus_ia_soldier_01_clothing_co.paa"
 	};
 };
+class Theseus_B_NAAF_Uniform_01_des_F: I_soldier_F
+{
+  	scope = protected;
+  	scopeCurator = private;
+	uniformClass = Theseus_U_B_NAAF_Uniform_01_des_F;
+	hiddenSelectionsTextures[] = {"\A3_Theseus\Characters_F_Theseus\Uniforms\Data\theseus_ia_soldier_01_clothing_des_co.paa"};
+};
+class Theseus_B_NAAF_Uniform_01_shortsleeve_des_F: I_Soldier_lite_F
+{
+  	scope = protected;
+  	scopeCurator = private;
+	uniformClass = Theseus_U_B_NAAF_Uniform_01_shortsleeve_des_F;
+	hiddenSelectionsTextures[] = {"\A3_Theseus\Characters_F_Theseus\Uniforms\Data\theseus_ia_soldier_01_clothing_des_co.paa"};
+};
