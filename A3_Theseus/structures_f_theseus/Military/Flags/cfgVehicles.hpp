90e7c1cdb6a9ef1675beccc49bd575ce1c47e1c7
Senicluxus <senicluxus@gmail.com>
2022-04-09 13:00:38 -0400

    Flag for the saviors of Altis

--- A3_Theseus/structures_f_theseus/Military/Flags/cfgVehicles.hpp
+++ A3_Theseus/structures_f_theseus/Military/Flags/cfgVehicles.hpp
@@ -74,4 +74,28 @@ class CfgVehicles
 			init = "(_this select 0) setFlagTexture '\A3_Theseus\Data_F_Theseus\Flags\flag_Bandits_CO.paa'";
 		};
 	};
+	class Flag_Minotaur_F: FlagCarrier
+	{
+		author = $STR_A3_A_Heliotrope;
+		class SimpleObject
+		{
+			eden = false;
+			animate[] =
+			{
+				{flag,0}
+			};
+			hide[] = {};
+			verticalOffset = 3.977;
+			verticalOffsetWorld = 0;
+			init = "''";
+		};
+		editorPreview = "\A3_Theseus\EditorPreviews_F_Theseus\Data\CfgVehicles\Flag_NAAF_F.jpg";
+		scope = public;
+		scopeCurator = public;
+		displayName = $STR_A3_T_CfgVehicles_Flag_Bandits_F0;
+		class EventHandlers
+		{
+			init = "(_this select 0) setFlagTexture '\A3_Theseus\Data_F_Theseus\Flags\flag_Minotaur_co.paa.paa'";
+		};
+	};
 };
