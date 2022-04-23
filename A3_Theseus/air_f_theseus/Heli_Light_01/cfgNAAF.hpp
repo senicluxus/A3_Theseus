569b173eee840c1fabb5b1946d841ec47c9276fe
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2022-04-23 15:57:50 +0100

    NAAF A/MH-9 Fixes

--- A3_Theseus/air_f_theseus/Heli_Light_01/cfgNAAF.hpp
+++ A3_Theseus/air_f_theseus/Heli_Light_01/cfgNAAF.hpp
@@ -1,6 +1,8 @@
 /* AAF */
 class Theseus_B_N_Heli_Light_01_F: I_Heli_Light_01_F
 {
+	scope = public;
+	scopeCurator = public;
 	side = TWest;
 	faction = Theseus_BLU_N_F;
 	crew = Theseus_B_N_Helipilot_F;
@@ -8,6 +10,8 @@ class Theseus_B_N_Heli_Light_01_F: I_Heli_Light_01_F
 };
 class Theseus_B_N_Heli_Light_01_dynamicLoadout_F: I_Heli_Light_01_dynamicLoadout_F
 {
+	scope = public;
+	scopeCurator = public;
     side = TWest;
 	faction = Theseus_BLU_N_F;
 	crew = Theseus_B_N_Helipilot_F;
